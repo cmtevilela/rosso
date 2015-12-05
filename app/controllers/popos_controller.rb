853ed8856d6e50d011f47d89b8a27c691cea6643
Rafael Lee <rafael.lee.87@gmail.com>
2015-12-05 18:58:41 -0200

    Atualização dos controllers/views dos popos

--- app/controllers/popos_controller.rb
+++ app/controllers/popos_controller.rb
@@ -1,40 +1,51 @@
 class PoposController < ApplicationController
 
-	def index
-		@popos = Popo.all
-	end
+  def index
+    @popos = Popo.all.order(:name)
 
-	def show
-		@popo = Popo.find(params[:id])
-	end
+    @popos_string = []
+    
+    @popos.each do |popo|
+      if popo.is_married?
+        @popos_string << "Casado"
+      else
+        @popos_string << "Vida Comum"
+      end
+    end
+  end
 
-	def new
-		@popo = Popo.new
-	end
+  def show
+    @popo = Popo.find(params[:id])
+  end
 
-	def popos_params
-		params.require(:popo).permit(:name)
-	end
+  def new
+    @popo = Popo.new
+  end
 
-	def create	
-		@popo = Popo.new(popos_params)		
+  def popos_params
+    params.require(:popo).permit(:name, :is_married)
+  end
 
-			if @popo.save
-				redirect_to popos_path, :notice => "A new Popo was created!"
-			else
-				render "new"
-			end
-	end
+  def create  
+    @popo = Popo.new(popos_params)    
 
-	def edit
-		
-	end
+    if @popo.save
+      redirect_to popos_path, :notice => "A new Popo was created!"
+    else
+      render "new"
+    end
+  end
 
-	def update
-		
-	end
+  def edit
+    @popo = Popo.find(params[:id])
+    
+  end
 
-	def destroy
-		
-	end
+  def update
+    
+  end
+
+  def destroy
+    
+  end
 end

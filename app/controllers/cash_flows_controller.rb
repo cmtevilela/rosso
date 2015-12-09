e63b4877c99720c3e68ef6bc0c0f06797d1d71d6
cmtevilela <tereza.cabocla@gmail.com>
2015-12-09 16:46:47 -0200

    Criado CashFlow Controller com views

--- app/controllers/cash_flows_controller.rb
+++ app/controllers/cash_flows_controller.rb
@@ -1,6 +1,52 @@
 class CashFlowsController < ApplicationController
+	before_action :set_cash_flow, only: [:show, :edit, :update, :destroy]
+    
 	def index
 		@cash_flows = CashFlow.all.order(:date)
-		
 	end
+
+	def show
+	end
+
+	def new
+	  @cash_flow = CashFlow.new	
+	end
+
+	def cash_flows_params
+		params.require(:cash_flow).permit(:description, :value, :date, :kind_id, :popo_id)
+	end
+
+	def create
+		@cash_flow = CashFlow.new(cash_flows_params)
+
+		if @cash_flow.save
+			redirect_to cash_flows_path, notice: "#{@cash_flow.description} lançado com sucesso!"
+		else
+			render "new"
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @cash_flow.update_attributes(cash_flows_params)
+			redirect_to cash_flows_path, notice: "#{@cash_flow.description} atualizado com sucesso!"
+		else
+			render "edit"
+		end
+	end
+
+	def destroy
+		@cash_flow.destroy
+		respond_to do |format|
+      format.html { redirect_to cash_flows_url, notice: "#{@cash_flow.description} removido com sucesso!" }
+      format.json { head :no_content }
+    end
+	end
+
+  private
+	  def set_cash_flow
+		  @cash_flow = CashFlow.find(params[:id])
+  	end
 end

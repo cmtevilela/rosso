4b1420221a529c25c414faddc79cdd31a27c5df6
Rafael Lee <rafael.lee.87@gmail.com>
2016-02-02 14:31:50 -0200

    something

--- app/controllers/current_user_controller.rb
+++ app/controllers/current_user_controller.rb
@@ -1,36 +1,37 @@
 class CurrentUserController < ApplicationController
   def index
-    @user_cashflows = CashFlow.where(popo_id: session[:popo_id])
-    @kinds = Kind.where(category_id: 1)
+    @popo = Popo.find(session[:popo_id])
+    @cash_flows = @popo.cash_flows.joins(:kind).order(:date, "kinds.name")
+    
+    @categories_sum = {}
 
-    @values_1 = []
-
-    @kinds.each do |kind|
-      @values_1.concat(CashFlow.where(kind_id: kind.id, 
-                                      popo_id: session[:popo_id]).pluck(:value))
-    end
-
-
+    Category.all.order(:name).each do |category|
+      values = @popo.cash_flows.where(kind_id: category.kinds.pluck(:id)).pluck(:value)
+      @categories_sum[category.name] = values.sum
+    end 
 
 
+    # @user_cashflows = CashFlow.where(popo_id: session[:popo_id])
+    # @kinds = Kind.where(category_id: 1)
 
+    # @values_1 = []
 
+    # @kinds.each do |kind|
+    #   @values_1.concat(CashFlow.where(kind_id: kind.id, 
+    #                                   popo_id: session[:popo_id]).pluck(:value))
+    # end
 
+    # @users_cash_cat1 = []
 
+    # @kinds.each do |kind|
+    #   @users_cash_cat1 << @user_cashflows.where(kind_id: kind.id)
+    # end
 
+    # @values = []
 
-
-    @users_cash_cat1 = []
-
-    @kinds.each do |kind|
-      @users_cash_cat1 << @user_cashflows.where(kind_id: kind.id)
-    end
-
-    @values = []
-
-    @user_cashflows.each do |cash_flow|
-      @values << cash_flow.value
-    end
+    # @user_cashflows.each do |cash_flow|
+    #   @values << cash_flow.value
+    # end
   end
 
   def view
--- app/models/cash_flow.rb
+++ app/models/cash_flow.rb
@@ -1,6 +1,6 @@
 class CashFlow < ActiveRecord::Base
-	belongs_to :popos
-	belongs_to :kinds
+	belongs_to :popo
+	belongs_to :kind
 
 	validates :value, presence: true, numericality: { less_than: 10000000, greater_than: -10000000 }
 	validates :date, presence: true
